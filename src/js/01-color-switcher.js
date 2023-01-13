0e7c0813a61cf2ed8afa6a1ee356054b661d2cda
Svitlana Samoliuk <101995368+Svetlana1208@users.noreply.github.com>
2023-01-13 18:07:08 +0200

    added task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+    body: document.querySelector('body'),
+};
 
+let intervalId = null;
+
+refs.btnStart.addEventListener('click', changeInterval);
+refs.btnStop.addEventListener('click', stopChanges);
+
+function changeInterval () {
+    intervalId = setInterval(changeBackground, 1000);
+    refs.btnStart.disabled = true;
+}
+
+function changeBackground() {
+    refs.body.style.background = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+function stopChanges() {
+    clearInterval(intervalId);
+    refs.btnStart.disabled = false;
+}
